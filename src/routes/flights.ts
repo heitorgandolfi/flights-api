cd41520d794a7a4c551696ddbccf7278d718800f
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-04-01 15:11:19 -0300

    feat: add flights routes with handlers

---
+++ src/routes/flights.ts
@@ -0,0 +1,37 @@
+import { Elysia, t } from "elysia";
+import { FlightsHandler } from "../handler/flights";
+
+export const flightsRoutes = (app: Elysia) =>
+  app
+    .get("/flights", async () => {
+      const flights = await FlightsHandler.getAll();
+
+      return flights;
+    })
+    .get("/flights/:id", async (req) => {
+      const { id } = req.params;
+      const flight = await FlightsHandler.getById(parseInt(id));
+
+      return flight;
+    })
+    .post(
+      "/flights",
+      async (req) => {
+        const data = req.body;
+        await FlightsHandler.create(data);
+      },
+      {
+        body: t.Object({
+          origin: t.String(),
+          destination: t.String(),
+          airline: t.String(),
+          price: t.Number(),
+          departure: t.String(),
+          arrival: t.String(),
+        }),
+      }
+    )
+    .delete("/flights/:id", async (req) => {
+      const { id } = req.params;
+      await FlightsHandler.deleteById(parseInt(id));
+    });
