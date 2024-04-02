1ebd06ab7c58f2c5e54bed8257c9ab8617f19db6
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2024-04-02 11:08:47 -0300

    style: separate routes visually

--- src/routes/bookings.ts
+++ src/routes/bookings.ts
@@ -1,27 +1,33 @@
 import { Elysia, t } from "elysia";
 import { BookingsHandler } from "../handler/bookings";
 
-export const bookingsRoutes = (app: Elysia) =>
-  app
-    .get("/bookings", async () => {
-      const bookings = await BookingsHandler.getAll();
+export const bookingsRoutes = (app: Elysia) => (
+  app.get("/bookings", async () => {
+    const bookings = await BookingsHandler.getAll();
 
-      return bookings;
-    }).get("/bookings/:id", async (req) => {
-      const { id } = req.params;
-      const booking = await BookingsHandler.getById(parseInt(id));
+    return bookings;
+  }),
 
-      return booking;
-    }).post("/bookings", async (req) => {
-      const data = req.body;
-      await BookingsHandler.create(data);
-    }, {
-      body: t.Object({
-        flightId: t.Number(),
-        userId: t.Number(),
-        seatNumber: t.String(),
-      })
-    }).delete("/bookings/:id", async (req) => {
-      const { id } = req.params;
-      await BookingsHandler.deleteById(parseInt(id));
+  app.get("/bookings/:id", async (req) => {
+    const { id } = req.params;
+    const booking = await BookingsHandler.getById(parseInt(id));
+
+    return booking;
+  }),
+
+  app.post("/bookings", async (req) => {
+    const data = req.body;
+    await BookingsHandler.create(data);
+  }, {
+    body: t.Object({
+      flightId: t.Number(),
+      userId: t.Number(),
+      seatNumber: t.String(),
     })
+  }),
+
+  app.delete("/bookings/:id", async (req) => {
+    const { id } = req.params;
+    await BookingsHandler.deleteById(parseInt(id));
+  })
+)
